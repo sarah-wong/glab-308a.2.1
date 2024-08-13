dfa4501e303ba68821f5bbc7c0cbde02b4e5f437
PerScholarSarah <rmandolin22@gmail.com>
2024-08-13 14:36:17 -0400

    adventurer.roll()

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion","artifact"]
+    inventory: ["sword", "potion","artifact"],
+    companion:{
+        name: "Leo",
+        type: "Cat",
+        companion:{
+            name: "Frank",
+            type: "Flea",
+            inventory: ["small hat","sunglasses"]
+        }
+    },
+    roll (mod=0) {
+        const result = Math.floor(1+Math.random()*20)+mod;
+        console.log(`${this.name} rolled a ${result}`);
+    }
 }
 
 function showItems(){
@@ -9,3 +22,10 @@ function showItems(){
         console.log(item);
     }
 }
+
+const roll1 = adventurer.roll();
+const roll2 = adventurer.roll(4);
+const roll3 = adventurer.roll(-2);
+
+console.log(`rolls: ${roll1}, ${roll2}, ${roll3}`);
+
