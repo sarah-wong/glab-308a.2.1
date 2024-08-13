f3d5b8389d449ddd4c7df7ca47fb62985ac7263d
PerScholarSarah <rmandolin22@gmail.com>
2024-08-13 14:39:45 -0400

    part 2 done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//------------------------Part 1
+
 const adventurer = {
     name: "Robin",
     health: 10,
@@ -29,3 +31,16 @@ const roll3 = adventurer.roll(-2);
 
 console.log(`rolls: ${roll1}, ${roll2}, ${roll3}`);
 
+//-----------------------------Part 2
+
+class Character {
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll(mod=0) {
+        const result = Math.floor(1+Math.random()*20)+mod;
+        console.log(`${this.name} rolled a ${result}`);
+    }
+}
