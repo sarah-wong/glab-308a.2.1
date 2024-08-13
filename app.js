47abad40b77004f61378b39f0605b199f75e007f
PerScholarSarah <rmandolin22@gmail.com>
2024-08-13 15:26:33 -0400

    reworked attack to work on non-Adventurers

--- app.js
+++ app.js
@@ -12,36 +12,37 @@ class Character {
 }
 
 class Adventurer extends Character {
-    constructor (name, role, power=0, armor=0, dodge=8) {
+    constructor (name, role, strength=0, armor=0) {
       super(name);
       this.role = role;
       this.inventory.push("bedroll", "50 gold coins");
-      this.stats = {atk:power, def:armor, evd:dodge};
+      this.stats = {atk:strength, def:armor};
     }
     scout () {
       console.log(`${this.name} is scouting ahead...`);
       super.roll();
     }
 
-    // avg. damage value =
-    //  (1 + target.stats.evd/20)*(this.stats.atk - target.stats.def)
     attack (target){
-        if(!target instanceof Adventurer){
+        if(!target instanceof Character){
             console.error(`${target} is not a valid attack target`);
             return;
         }
         console.log(`${this.name} is attacking ${target.name}...`);
-        const roll = super.roll(this.stats.atk)
-        if(roll >= target.stats.evd){
-            const dmg = roll - target.stats.def;
+        const roll = super.roll(this.stats.atk);
+        let dmg = roll;
+        if(target instanceof Adventurer){
+            dmg -= target.stats.def;
+        }
+        if(dmg > 0){
             target.health -= dmg;
-            console.log(`Hit! ${target.name} takes ${dmg} damage!`);
+            console.log(`Hit! ${target.name} took ${dmg} damage!`);
         }
         else{
-            console.log(`Miss! ${target.name} evaded the attack!`);
+            console.log(`Miss! ${target.name} evaded!`);
         }
         if(target.health <= 0){
-            console.log(`${target.name} was knocked out!`);
+            console.log(`${target.name} took mortal damage!`);
         }
         else{
             console.log(`${target.name} was reduced to ${target.health} HP`);
@@ -49,7 +50,7 @@ class Adventurer extends Character {
     }
 }
 
-const hero = new Adventurer("Robin","Fighter",4,3);
-const enemy = new Adventurer("Goblin","Thief",0,0);
+const hero = new Adventurer("Robin","Fighter",4,10);
+const enemy = new Character("Goblin");
 
 hero.attack(enemy);
