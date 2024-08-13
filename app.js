270bb0a9e1c176f191abcf533d062a04fc10277e
PerScholarSarah <rmandolin22@gmail.com>
2024-08-13 15:17:55 -0400

    added Adventurer.attack()

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-//------------------------Part 1
-
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion","artifact"],
-    companion:{
-        name: "Leo",
-        type: "Cat",
-        companion:{
-            name: "Frank",
-            type: "Flea",
-            inventory: ["small hat","sunglasses"]
-        }
-    },
-    roll (mod=0) {
-        const result = Math.floor(1+Math.random()*20)+mod;
-        console.log(`${this.name} rolled a ${result}`);
-    }
-}
-
-function showItems(){
-    for (const item of adventurer.inventory) {
-        console.log(item);
-    }
-}
-
-const roll1 = adventurer.roll();
-const roll2 = adventurer.roll(4);
-const roll3 = adventurer.roll(-2);
-
-console.log(`rolls: ${roll1}, ${roll2}, ${roll3}`);
-
-//-----------------------------Part 2
-
 class Character {
     constructor(name){
         this.name = name;
@@ -41,19 +6,50 @@ class Character {
     }
     roll(mod=0) {
         const result = Math.floor(1+Math.random()*20)+mod;
-        console.log(`${this.name} rolled a ${result}`);
+        console.log(`${this.name} rolled ${result}`);
+        return result;
     }
 }
 
 class Adventurer extends Character {
-    constructor (name, role, stats = {power:0, armor:0, speed:0}) {
+    constructor (name, role, power=0, armor=0, dodge=8) {
       super(name);
       this.role = role;
       this.inventory.push("bedroll", "50 gold coins");
-      this.stats = stats;
+      this.stats = {atk:power, def:armor, evd:dodge};
     }
     scout () {
       console.log(`${this.name} is scouting ahead...`);
       super.roll();
     }
-  }
+
+    // avg. damage value =
+    //  (1 + target.stats.evd/20)*(this.stats.atk - target.stats.def)
+    attack (target){
+        if(!target instanceof Adventurer){
+            console.error(`${target} is not a valid attack target`);
+            return;
+        }
+        console.log(`${this.name} is attacking ${target.name}...`);
+        const roll = super.roll(this.stats.atk)
+        if(roll >= target.stats.evd){
+            const dmg = roll - target.stats.def;
+            target.health -= dmg;
+            console.log(`Hit! ${target.name} takes ${dmg} damage!`);
+        }
+        else{
+            console.log(`Miss! ${target.name} evaded the attack!`);
+        }
+        if(target.health <= 0){
+            console.log(`${target.name} was knocked out!`);
+        }
+        else{
+            console.log(`${target.name} was reduced to ${target.health} HP`);
+        }
+    }
+}
+
+const hero = new Adventurer("Robin","Fighter",4,3);
+const enemy = new Adventurer("Goblin","Thief",0,0);
+
+hero.attack(enemy);
